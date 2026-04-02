d54c56a17e875e9186874899525ae3ca4d54f992
StehvenObandoUcc <stehven.obando@campusucc.edu.co>
2026-04-02 15:27:36 -0500

    ai-suggestions: drop obsolete price import in scanner test

--- src/test/java/com/veltro/inventory/controller/ScannerControllerTest.java
+++ src/test/java/com/veltro/inventory/controller/ScannerControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
