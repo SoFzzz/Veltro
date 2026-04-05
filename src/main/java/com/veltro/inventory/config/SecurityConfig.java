604cda3afbec2f2ee3a6912eaf87240df6f164b4
SoFzz <paula.gonzalezz@campusucc.edu.co>
2026-04-05 08:14:40 -0500

    Test nine

--- src/main/java/com/veltro/inventory/config/SecurityConfig.java
+++ src/main/java/com/veltro/inventory/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
