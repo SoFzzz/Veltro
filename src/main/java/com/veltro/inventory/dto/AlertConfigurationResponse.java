f56d021f2d320803e738f485261344cbc2e55297
StehvenObandoUcc <stehven.obando@campusucc.edu.co>
2026-04-09 09:02:24 -0500

    Refactor

--- src/main/java/com/veltro/inventory/dto/AlertConfigurationResponse.java
+++
@@ -1,8 +0,0 @@
-package com.veltro.inventory.dto;
-
-public record AlertConfigurationResponse(
-        Long productId,
-        Integer criticalStock,
-        Integer minStock,
-        Integer overstockThreshold) {
-}
