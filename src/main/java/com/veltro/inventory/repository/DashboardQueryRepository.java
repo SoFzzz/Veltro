0472274db65f4ffbc1e02e143f22ab5a5c2b6696
SoFzzz <paula.gonzalezz@campusucc.edu.co>
2026-04-16 19:49:22 -0500

    Refactor

--- src/main/java/com/veltro/inventory/repository/DashboardQueryRepository.java
+++ src/main/java/com/veltro/inventory/repository/DashboardQueryRepository.java
@@ -1,6 +1,6 @@
 package com.veltro.inventory.repository;
 
-import com.veltro.inventory.dto.DashboardResponse;
+import com.veltro.inventory.dto.dashboard.DashboardResponse;
 import com.veltro.inventory.model.AlertType;
 import com.veltro.inventory.model.SaleStatus;
 import jakarta.persistence.EntityManager;
@@ -219,3 +219,4 @@ public class DashboardQueryRepository implements com.veltro.inventory.service.Da
                 .toList();
     }
 }
+
