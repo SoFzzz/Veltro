e9ceb906c897e0deda39492f1f6fe2fee1044f13
StehvenObandoUcc <stehven.obando@campusucc.edu.co>
2026-05-07 20:53:04 -0500

    b5

--- src/test/java/com/veltro/inventory/state/PurchaseOrderVoidedStateTest.java
+++ src/test/java/com/veltro/inventory/state/PurchaseOrderVoidedStateTest.java
@@ -48,7 +48,7 @@ class PurchaseOrderVoidedStateTest {
 
         assertThatThrownBy(() -> state.addItem(order, detail))
                 .isInstanceOf(InvalidStateTransitionException.class)
-                .hasMessageContaining("Cannot add items to purchase order in VOIDED status");
+                .hasMessageContaining("error.state.po.add_item_denied");
     }
 
     @Test
@@ -56,7 +56,7 @@ class PurchaseOrderVoidedStateTest {
     void removeItem_voidedState_throwsInvalidStateTransition() {
         assertThatThrownBy(() -> state.removeItem(order, 1L))
                 .isInstanceOf(InvalidStateTransitionException.class)
-                .hasMessageContaining("Cannot remove items from purchase order in VOIDED status");
+                .hasMessageContaining("error.state.po.remove_item_denied");
     }
 
     @Test
@@ -68,7 +68,7 @@ class PurchaseOrderVoidedStateTest {
 
         assertThatThrownBy(() -> state.receivePartial(order, receivedItems))
                 .isInstanceOf(InvalidStateTransitionException.class)
-                .hasMessageContaining("Cannot receive items for purchase order in VOIDED status");
+                .hasMessageContaining("error.state.po.receive_denied");
     }
 
     @Test
@@ -76,7 +76,7 @@ class PurchaseOrderVoidedStateTest {
     void voidOrder_voidedState_throwsInvalidStateTransition() {
         assertThatThrownBy(() -> state.voidOrder(order))
                 .isInstanceOf(InvalidStateTransitionException.class)
-                .hasMessageContaining("Cannot void purchase order in VOIDED status");
+                .hasMessageContaining("error.state.po.void_denied");
     }
 
     @Test
@@ -86,6 +86,6 @@ class PurchaseOrderVoidedStateTest {
 
         assertThatThrownBy(() -> state.receivePartial(order, emptyList))
                 .isInstanceOf(InvalidStateTransitionException.class)
-                .hasMessageContaining("Cannot receive items for purchase order in VOIDED status");
+                .hasMessageContaining("error.state.po.receive_denied");
     }
 }
