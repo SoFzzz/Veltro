77bb4e8a3046dbc08792f52b15c89d6956e14d9e
StehvenObandoUcc <stehven.obando@campusucc.edu.co>
2026-05-19 12:24:15 -0500

    last

---
+++ src/test/java/com/veltro/inventory/BaseIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.veltro.inventory;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>(
+                    DockerImageName.parse("ankane/pgvector:pg16")
+                            .asCompatibleSubstituteFor("postgres"))
+                    .withDatabaseName("veltro_test")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
