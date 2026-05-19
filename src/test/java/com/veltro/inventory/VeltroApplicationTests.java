ab03336d1df74dde225707e6880bc8bce4069d4a
SoFzzz <paula.gonzalezz@campusucc.edu.co>
2026-05-19 14:23:25 -0500

    last

--- src/test/java/com/veltro/inventory/VeltroApplicationTests.java
+++ src/test/java/com/veltro/inventory/VeltroApplicationTests.java
@@ -1,12 +1,8 @@
 package com.veltro.inventory;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("Requiere PostgreSQL real, no compatible con H2 en tests")
-@SpringBootTest
-class VeltroApplicationTests {
+class VeltroApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
